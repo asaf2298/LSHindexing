9f9a89818db0185068fb43146565224e64e7add4
asaf2298 <120143624+asaf2298@users.noreply.github.com>
2026-07-15 00:15:43 +0300

    Refactor query method in LSHIndex class

--- search.py
+++ search.py
@@ -31,7 +31,7 @@ def cosine_similarity(a, b):
 class LSHIndex:
     def __init__(self, vectors, num_tables, num_bits, **kwargs):
         if not isinstance(vectors, np.ndarray) or not isinstance(num_tables, int) or not isinstance(num_bits, int):
-          raise ValueError("vectors must be numpy arrays and num_bits and num_tablemust be int type")
+            raise ValueError("vectors must be numpy arrays and num_bits and num_tablemust be int type")
         if vectors.dtype != np.float32:
             raise ValueError("vectors must be float32")
         self.vectors = vectors
@@ -41,7 +41,7 @@ class LSHIndex:
         rng = np.random.default_rng(seed)
         self.planes = rng.normal(size=(num_tables, num_bits, vectors.shape[1])).astype(np.float32)
         self.tables = self._build_tables()
-        
+
     def _build_tables(self):
         tables = []
         for idx in range(self.num_tables):
@@ -55,26 +55,26 @@ class LSHIndex:
                 table[key].append(n)
             tables.append(dict(table))
         return tables
-  
-    def query(self, q_vec, k):       
-      if k <=0:
-          return []          
-      matched_hashes = set()
-      heap = []
-      for idx in range(self.num_tables):
-        dot_product_query = (np.dot(self.planes[idx], q_vec) >= 0).astype(int) 
-        key_query = sum(dot_product_query[i] * (2 ** i) for i in range(self.num_bits))
-        matched_hashes.update(self.tables[idx].get(key_query, []))          
-      for n in matched_hashes: # כיוון שמחפשים קי אייברים שזה פחות שווה לגודל המערכת, יעיל יותר למיין קי ערכים תוך כדי שלפיה
-          score = cosine_similarity(self.vectors[n], q_vec)
-          item = (score, n)   
-          if len(heap) < k:
-              heapq.heappush(heap, item)
-          else:
-              if score > heap[0][0]:
-                  heapq.heapreplace(heap, item)      
-      return sorted(heap, reverse=True) #O(k log k)
-       
+
+    def query(self, q_vec, k):
+        if k <=0:
+            return []
+        matched_hashes = set()
+        heap = []
+        for idx in range(self.num_tables):
+            dot_product_query = (np.dot(self.planes[idx], q_vec) >= 0).astype(int)
+            key_query = sum(dot_product_query[i] * (2 ** i) for i in range(self.num_bits))
+            matched_hashes.update(self.tables[idx].get(key_query, []))
+        for n in matched_hashes: # כיוון שמחפשים קי אייברים שזה פחות שווה לגודל המערכת, יעיל יותר למיין קי ערכים תוך כדי שלפיה
+            score = cosine_similarity(self.vectors[n], q_vec)
+            item = (score, n)
+            if len(heap) < k:
+                heapq.heappush(heap, item)
+            else:
+                if score > heap[0][0]:
+                    heapq.heapreplace(heap, item)
+        return sorted(heap, reverse=True) #O(k log k)
+
 def search(query, index, encoder, movies, k=5):
     encoded = encoder.encode(query)
     encoded = np.asarray(encoded, dtype=np.float32)
@@ -85,3 +85,4 @@ def search(query, index, encoder, movies, k=5):
         movie["score"] = float(score)
         res.append(movie)
     return res
+
